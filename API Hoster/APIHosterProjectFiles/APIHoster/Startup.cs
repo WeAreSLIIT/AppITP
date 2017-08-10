07f47c9fb2ebf0dce7f1db0f0d951a6c08dbd211
Chathuranga Basnayake <chathurangabasnayake@outlook.com>
2017-08-10 10:58:29 +0530

    Upload CSB API Hoster

---
+++ API Hoster/APIHosterProjectFiles/APIHoster/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Owin;
+using System.Web.Http;
+
+namespace APIHoster
+{
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            //config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            app.UseWebApi(config);
+        }
+    }
+}
