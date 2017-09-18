39047f50ad7728b84e46eebfae78feb0abff3dd3
Chathuranga Basnayake <chathurangabasnayake@outlook.com>
2017-09-18 16:48:17 +0530

    Strat Web API

---
+++ Bassa/Inventry Management System - ITP/WebAPI/App_Start/MappingConfig.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.Core.Domain;
+using WebAPI.Controllers.Resources;
+using System.Linq;
+
+namespace WebAPI.App_Start
+{
+    public static class AutoMappingConfig
+    {
+        public static void Configure()
+        {
+            AutoMapper.Mapper.Initialize(config =>
+            {
+                #region Domain Model to API Resources
+
+                config.CreateMap<Invoice, InvoiceResource>()
+                    .ForMember(dest => dest.InvoiceId, opt => opt.MapFrom(sour => sour.PublicID));
+
+                #endregion
+
+                #region API Resources to Domain Model
+
+                config.CreateMap<InvoiceResource, Invoice>()
+                    .ForMember(dest => dest.PublicID, opt => opt.MapFrom(sour => sour.InvoiceId))
+                    .ForMember(dest => dest.InvoiceProducts, opt => opt.MapFrom(sour => sour.Products.Select(id => new InvoiceProduct { ProductID = id })));
+
+                #endregion
+            });
+        }
+    }
+}
---
+++ Bassa/Inventry Management System - ITP/WebAPI/Controllers/Resources/InvoiceResource.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Controllers.Resources
+{
+    public class InvoiceResource
+    {
+        public string InvoiceId { get; set; }
+        public long Time { get; set; }
+        public float FullPayment { get; set; }
+        public float Discount { get; set; }
+        public float Payed { get; set; }
+        public float Balance { get; set; }
+
+        public long IssuedBy { get; set; }
+
+        public long? PurchasedBy { get; set; }
+        //public InvoiceDeal InvoiceDeal { get; set; }
+
+        public ICollection<int> Products { get; set; }
+        public ICollection<int> PaymentMethods { get; set; }
+
+        public InvoiceResource()
+        {
+            Products = new HashSet<int>();
+            PaymentMethods = new HashSet<int>();
+        }
+    }
+}
